6f48ab87fd71ebbe5f8c8dd7e116bc9e4dabaf8e
neru <152752583+neeeruuu@users.noreply.github.com>
2024-11-03 15:43:59 -0300

    fix: actually push assume changes (oops)

--- src/nerutils/assert.h
+++ src/nerutils/assert.h
@@ -2,27 +2,38 @@
 
 void nutil_error(const char* err);
 
-#ifdef _DEBUG
-#define ACTUAL_STRINGIZE(x) #x
-#define STRINGIZE(x) ACTUAL_STRINGIZE(x)
+#ifdef _MSC_VER
+    #define ASSUME(cond) __assume(cond)
+#elif defined(__GNUC__) || defined(__clang__)
+    #define ASSUME(cond) __builtin_assume(cond)
+#else
+    #define ASSUME(cond)                                                                                               \
+        do                                                                                                             \
+        {                                                                                                              \
+        } while (0)
+#endif
 
-#define ASSERT(cond, msg)                                                                                          \
+#ifdef _DEBUG
+    #define ACTUAL_STRINGIZE(x) #x
+    #define STRINGIZE(x) ACTUAL_STRINGIZE(x)
+
+    #define ASSERT(cond, msg)                                                                                          \
         do                                                                                                             \
         {                                                                                                              \
             if (!(cond))                                                                                               \
             {                                                                                                          \
                 nutil_error(msg "\nfile:" __FILE__ "\nline:" STRINGIZE(__LINE__));                                     \
             }                                                                                                          \
-        } while (false)
+        } while (false) ASSUME(cond);
 #else
-#define ASSERT(cond, msg)                                                                                          \
+    #define ASSERT(cond, msg)                                                                                          \
         do                                                                                                             \
         {                                                                                                              \
             if (!(cond))                                                                                               \
             {                                                                                                          \
                 nutil_error(msg);                                                                                      \
             }                                                                                                          \
-        } while (false)
+        } while (false) ASSUME(cond);
 #endif
 
 #define PANIC(msg) nutil_error(msg);
